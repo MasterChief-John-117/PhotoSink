8252cf467b0e5f898feef13996990330471c62b6
Galen G <github@mastrchef.rocks>
2019-12-14 01:57:17 -0500

    Add location.hash handling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+}
+interface IState {
+  location: string,
+}
+
+class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      location: window.location.hash,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("hashchange", () => { 
+      this.setState({location: window.location.hash});
+    });
+  }
+
+  render () {
+      return (
+      <div className="App">
+        <h1>{this.state.location}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
